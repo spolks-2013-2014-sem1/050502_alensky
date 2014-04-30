cd4df10ab4777212043b7dcb4de0d8f30288ea87
ArthurAlensky <ArthurAlensky@gmail.com>
2014-04-30 18:58:05 +0300

    Add last version of Broadcast( lab3_2/session_v3.c ) Add folder for Multicast(lab4_2)

--- lab3_2/session.c
+++ lab3_2/session.c
@@ -10,6 +10,8 @@
  */
 
 #include <stdio.h>
+#include <pthread.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -18,15 +20,33 @@
 
 #define MAXBUF 65536
 
+pthread_t tid[2];
+
+void listen_thread( void *arg )
+{
+	char buffer_rd[MAXBUF];
+	int buflen_rd;
+	
+	status = recvfrom(sock_rd, buffer_rd, buflen_rd, 0, (struct sockaddr *)&sock_in_rd, &sinlen_rd);
+}
+
+void speak_thread( void *arg )
+{
+	char buffer_wr[MAXBUF];
+	int buflen_wr;
+	
+	status = sendto(sock_wr, buffer_wr, buflen_wr, 0, (struct sockaddr *)&sock_in_wr, sinlen_wr);
+}
+
 int main( int argc, char** argv)
 {
-  int sock_rd, sock_wr, status, buflen_rd, buflen_wr;
+  int sock_rd, sock_wr, status; 
   unsigned sinlen_rd, sinlen_wr;
-  char buffer_wr[MAXBUF];
-  char buffer_rd[MAXBUF];
+  
+  
   struct sockaddr_in sock_in_rd, sock_in_wr;
   int yes = 1;
-  int port = 32943;
+  int port = 32949;
   
   sinlen_wr = sizeof(struct sockaddr_in);
   memset(&sock_in_wr, 0, sinlen_wr);
@@ -37,16 +57,16 @@ int main( int argc, char** argv)
   
   sock_rd = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-  sock_wr = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  sock_wr = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
   
   sock_in_rd.sin_addr.s_addr = htonl(INADDR_ANY);
-  sock_in_rd.sin_port = htons(    /*argv[1]  ?  atoi(argv[1]) :*/ port   ); /* port number */
+  sock_in_rd.sin_port = htons(  argv[1]  ?  atoi(argv[1]) : port   );
   sock_in_rd.sin_family = PF_INET;
 
   
   sock_in_wr.sin_addr.s_addr = htonl(INADDR_ANY);
-  sock_in_wr.sin_port = htons( 0  ); /* port number */
-  sock_in_wr.sin_family = PF_INET;
+  sock_in_wr.sin_port = htons( 0  );
+  sock_in_wr.sin_family = AF_INET;
   
   
   status = bind(sock_rd, (struct sockaddr *)&sock_in_rd, sinlen_rd);
@@ -56,33 +76,25 @@ int main( int argc, char** argv)
   printf("Bind Status Writer = %d\n", status);
   
   status = setsockopt(sock_wr, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(int) );
-  status = getsockname(sock_wr, (struct sockaddr *)&sock_in_wr, &sinlen_wr);
+  status = getsockname(sock_rd, (struct sockaddr *)&sock_in_rd, &sinlen_rd);
   printf("Sock port %d\n",htons(sock_in_rd.sin_port));
 
-  sock_in_wr.sin_addr.s_addr=htonl(-1); /* send message to 255.255.255.255 */
-  sock_in_wr.sin_port = sock_in_rd.sin_port; /*htons( port   );*/ /* port number */
-  sock_in_wr.sin_family = PF_INET;
+  sock_in_wr.sin_addr.s_addr=htonl(-1);
+  sock_in_wr.sin_port = htons(atoi(argv[1]));
+  sock_in_wr.sin_family = AF_INET;
   
-  sprintf(buffer_wr, "Message");
-  buflen_wr = strlen(buffer_wr);
+  
+  
+  pthread_create( &tid[0], NULL, listen_thread, NULL  );
+  pthread_create( &tid[1], NULL, speak_thread, NULL  );
   
   while(1)
   {
     buflen_wr = MAXBUF;
     memset(buffer_wr, 0, buflen_wr);
     
-    status = sendto(sock_wr, buffer_wr, buflen_wr, 0, (struct sockaddr *)&sock_in_wr, sinlen_wr);
-    
-    status = recvfrom(sock_rd, buffer_rd, buflen_rd, 0, (struct sockaddr *)&sock_in_rd, &sinlen_rd);
     
     
-    if(strcmp(buffer_rd, buffer_wr))
-    {
-      printf("!!!! FAIL %s != %s\n", buffer_rd, buffer_wr);
-      break;
-    }
-    
-    printf("%s = %s\n", buffer_wr, buffer_rd);
     
   }
 
@@ -91,4 +103,4 @@ int main( int argc, char** argv)
   
   shutdown(sock_rd, 2);
   close(sock_rd);
-}  
+}   
---
+++ lab3_2/session_v3.c
@@ -0,0 +1,219 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pthread.h>
+#include <string.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <signal.h>
+#include <net/if.h>
+#include <linux/ip.h>
+#include <linux/icmp.h>
+#include <sys/ioctl.h>
+
+
+
+#define MAXBUF 65536
+
+int sock_rd, sock_wr;
+struct sockaddr_in sock_in_rd, sock_in_wr;
+char client[10][1000];
+int clientsNum = 0;
+char *enter = "*_EC";
+
+int kbhit(void)
+{
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+ 
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+ 
+  ch = getchar();
+  
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+ 
+  if(ch != EOF)
+  {
+    if( ch == '-')
+    {
+      return 2;
+    }
+    else{
+      ungetc(ch, stdin);
+      return 1;
+    }
+  }
+ 
+  return 0;
+}
+
+
+char* getip()
+{
+	 int fd;
+	 struct ifreq ifr;
+
+	 fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	 ifr.ifr_addr.sa_family = AF_INET;
+
+	 strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
+
+	 ioctl(fd, SIOCGIFADDR, &ifr);
+
+	 close(fd);
+
+	 return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+}
+
+void listen_thread( void *arg )
+{
+	char buffer_rd[MAXBUF];
+	int  buflen_rd, status;
+	unsigned sinlen_rd = sizeof(struct sockaddr_in);
+	
+	while(1)
+	{
+		buflen_rd = MAXBUF;
+		memset(buffer_rd, 0, buflen_rd);
+		status = recvfrom(sock_rd, buffer_rd, buflen_rd, 0, (struct sockaddr *)&sock_in_rd, &sinlen_rd);
+		
+		if(!strcmp(buffer_rd, enter))
+		{
+		  buflen_rd = MAXBUF;
+		  memset(buffer_rd, 0, buflen_rd);
+		  status = recvfrom(sock_rd, buffer_rd, buflen_rd, 0, (struct sockaddr *)&sock_in_rd, &sinlen_rd);
+		  strcpy(client[clientsNum++], buffer_rd);
+		}
+		else printf("%s\n", buffer_rd);
+	}
+}
+
+void speak_thread( void *arg )
+{
+	char buffer_wr[MAXBUF];
+	int buflen_wr, status = 0;
+	unsigned sinlen_wr = sizeof(struct sockaddr_in);
+	
+	while(1)
+	{
+		int ch = kbhit();
+		if(ch)
+		{
+		  if( ch == 2 )
+		  {
+		    int i = 0;
+		    for( i =0; i < clientsNum; i++ )
+		      printf("%s\n", client[i] );
+		  }
+		  else{
+		    memset(buffer_wr, 0, MAXBUF);
+		    fflush(stdin);
+		    gets(buffer_wr);
+		    buflen_wr = strlen(buffer_wr);    
+		    status = sendto(sock_wr, buffer_wr, buflen_wr, 0, (struct sockaddr *)&sock_in_wr, sinlen_wr);
+		  }
+		}
+		
+	}
+}
+
+char* get_broadcast( char* name )
+{
+	struct ifaddrs *ifaddr, *ifa;
+	int family, s, n, getstatus = 0;
+	char host[NI_MAXHOST];
+
+	if (getifaddrs(&ifaddr) == -1) 
+	{
+		perror("getifaddrs");
+		exit(-1);
+	}
+
+
+	for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
+
+		if (ifa->ifa_addr == NULL)
+			continue;
+
+		if (strcmp(ifa->ifa_name, name)!=0)
+			continue;
+		
+		return inet_ntoa ((( struct sockaddr_in*)ifa->ifa_broadaddr)->sin_addr);
+	}
+           
+	return NULL;
+}
+
+int main( int argc, char** argv)
+{
+  unsigned sinlen_rd, sinlen_wr;
+  int yes = 1, port = 0, status = 0, arg, reuse = 1;
+  pthread_t tid[2];
+  
+  sinlen_wr = sizeof(struct sockaddr_in);
+  memset(&sock_in_wr, 0, sinlen_wr);
+
+  sinlen_rd = sizeof(struct sockaddr_in);
+  memset(&sock_in_rd, 0, sinlen_rd);
+  
+  sock_rd = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  sock_wr = socket (PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  
+  sock_in_rd.sin_addr.s_addr = htonl(INADDR_ANY);
+  sock_in_rd.sin_port = htons(    argv[1]  ?  atoi(argv[1]) : port   );
+  sock_in_rd.sin_family = PF_INET;
+
+  sock_in_wr.sin_addr.s_addr = htonl(INADDR_ANY);
+  sock_in_wr.sin_port = htons( 0 );
+  sock_in_wr.sin_family = PF_INET;
+  
+  status = bind(sock_rd, (struct sockaddr *)&sock_in_rd, sinlen_rd);
+  printf("Bind Status Reader = %d\n", status);
+
+  status = bind(sock_wr, (struct sockaddr *)&sock_in_wr, sinlen_wr);
+  printf("Bind Status Writer = %d\n", status);
+  
+  status = setsockopt(sock_wr, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(int) );
+  setsockopt(sock_wr, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
+  setsockopt(sock_rd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
+  status = getsockname(sock_rd, (struct sockaddr *)&sock_in_rd, &sinlen_rd);
+  printf("Sock port %d\n",htons(sock_in_rd.sin_port));
+
+  sock_in_wr.sin_addr.s_addr=htonl(-1);
+  sock_in_wr.sin_port = sock_in_rd.sin_port;
+  sock_in_wr.sin_family = PF_INET;
+  
+  char* ip = getip();
+  status = sendto(sock_wr, enter, strlen(enter), 0, (struct sockaddr *)&sock_in_wr, sinlen_wr);
+  status = sendto(sock_wr, ip, strlen(ip), 0, (struct sockaddr *)&sock_in_wr, sinlen_wr);
+  
+  
+  pthread_create( &tid[0], NULL, listen_thread, (void*) arg );
+  pthread_create( &tid[1], NULL, speak_thread, (void*) arg );
+
+  pthread_join( tid[0], NULL); 
+  pthread_join( tid[1], NULL); 
+  
+  shutdown(sock_wr, 2);
+  close(sock_wr);
+  
+  shutdown(sock_rd, 2);
+  close(sock_rd);
+}  
